cb19c66d5ce7a46e652f7d90bd9e3615c82e1e49
Markus Schulz <msc@intentus.de>
2024-11-06 08:45:48 +0100

    fix: callTimeout aborts removed
    callTimeout is the maximum time for a download.
    We don't want to limit this (and 2min is much too short)

--- src/main/java/de/a0zero/rssdl/download/Progress.java
+++ src/main/java/de/a0zero/rssdl/download/Progress.java
@@ -26,7 +26,7 @@ public class Progress {
 	public Response run(URL url, ProgressBar progressBar) throws IOException {
 		Request request = new Request.Builder().url(url).build();
 		OkHttpClient client = new OkHttpClient.Builder()
-				.readTimeout(2, TimeUnit.MINUTES).callTimeout(2, TimeUnit.MINUTES)
+				.readTimeout(2, TimeUnit.MINUTES)
 				.followRedirects(true)
 				.addNetworkInterceptor(chain -> {
 					if (progressBar != null) {
